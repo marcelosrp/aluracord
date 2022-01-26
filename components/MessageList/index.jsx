1542f2334d8c5d29e1288959e13e796f5289ba2e
Marcelo Soares <soares.srp@gmail.com>
2022-01-26 10:43:04 -0300

    Aula 3: Chat offline? Aprimorando as habilidades com State

---
+++ components/MessageList/index.jsx
@@ -0,0 +1,65 @@
+import { Box, Text, Image } from '@skynexui/components'
+import appConfig from '../../config.json'
+
+export default function MessageList(props) {
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflowY: 'scroll',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        flex: 1,
+        color: appConfig.theme.colors.neutrals['000'],
+        marginBottom: '16px',
+      }}
+    >
+      {props.mensagens.map(mensagem => {
+        return (
+          <Text
+            key={mensagem.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: '5px',
+              padding: '6px',
+              marginBottom: '12px',
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+            key={mensagem.id}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: '8px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: '20px',
+                  height: '20px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  marginRight: '8px',
+                }}
+                src={`https://github.com/vanessametonini.png`}
+              />
+              <Text tag="strong">{mensagem.de}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: '10px',
+                  marginLeft: '8px',
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {mensagem.texto}
+          </Text>
+        )
+      })}
+    </Box>
+  )
+}
--- pages/chat.jsx
+++ pages/chat.jsx
@@ -1,3 +1,98 @@
-export default function PaginaChat() {
-  return <h1>Página do Chat</h1>
+import React, { useState } from 'react'
+import { Box, TextField } from '@skynexui/components'
+import Header from '../components/Header'
+import MessageList from '../components/MessageList'
+import appConfig from '../config.json'
+
+export default function ChatPage() {
+  const [mensagem, setMensagem] = useState('')
+  const [listaMensagens, setListaMensagens] = useState([])
+
+  function handleNovaMensagem(event) {
+    const mensagemObj = {
+      id: listaMensagens.length + 1,
+      de: 'marcelosrp',
+      texto: mensagem,
+    }
+
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      setListaMensagens(prevState => [mensagemObj, ...prevState])
+      setMensagem('')
+    }
+  }
+
+  return (
+    <Box
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundBlendMode: 'multiply',
+        color: appConfig.theme.colors.neutrals['000'],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: '100%',
+          maxWidth: '95%',
+          maxHeight: '95vh',
+          padding: '32px',
+        }}
+      >
+        <Header />
+
+        <Box
+          styleSheet={{
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px',
+          }}
+        >
+          <MessageList mensagens={listaMensagens} />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+              value={mensagem}
+              onChange={({ target }) => setMensagem(target.value)}
+              onKeyPress={event => handleNovaMensagem(event)}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
